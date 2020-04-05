e1ddead8f8081dcabdc44b9d8bc69d27c1367d2f
Moreno Gentili <brightsoul@tiscali.it>
2020-04-05 14:21:16 +0200

    Primo commit

---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Threading.Tasks;
+using OnvifDemo.Proxies.Ptz;
+using System.Text;
+using OnvifDemo.Proxies.Device;
+using OnvifDemo.Proxies.Media;
+using System.Linq;
+using System;
+
+namespace OnvifDemo
+{
+    class Program
+    {
+        const string deviceEndpoint = "http://192.168.1.22:10000/onvif/device_service"; //Modifica questo
+
+        static async Task Main(string[] args)
+        {
+            string mediaEndpoint = await GetEndpointForService(deviceEndpoint, "http://www.onvif.org/ver10/media/wsdl");
+            Console.WriteLine($"Il media endpoint è: {mediaEndpoint}");
+
+            string ptzEndpoint = await GetEndpointForService(deviceEndpoint, "http://www.onvif.org/ver20/ptz/wsdl");
+            Console.WriteLine($"Il PTZ endpoint è: {ptzEndpoint}");
+
+            string profileName = await GetFirstProfileName(mediaEndpoint);
+            Console.WriteLine($"Il nome del primo profilo esposto è: {profileName}");
+
+            //Spostiamo l'inquadratura
+            float x = 0.5f;
+            float y = 0.5f;
+            float zoom = 0.5f;
+            await AbsoluteMoveAsync(ptzEndpoint, profileName, x, y, zoom);
+            Console.WriteLine($"L'inquadratura è stata spostata su x={x} y={y} zoom={zoom}");
+
+            Console.WriteLine("Premere INVIO per terminare");
+            Console.ReadLine();
+        }
+
+        private async static Task<string> GetEndpointForService(string deviceEndpoint, string serviceNamespace)
+        {
+            var client = CreateDeviceClient(deviceEndpoint);
+            var services = await client.GetServicesAsync(IncludeCapability: false);
+            var endpoint = services.Service.Single(service => service.Namespace == serviceNamespace);
+            return endpoint.XAddr;
+        }
+
+        private async static Task<string> GetFirstProfileName(string mediaEndpoint)
+        {
+            var client = CreateMediaClient(mediaEndpoint);
+            var profiles = await client.GetProfilesAsync();
+            return profiles.Profiles.First().Name;
+        }
+
+        private static async Task AbsoluteMoveAsync(string ptzEndpoint, string profileName, float x, float y, float zoom)
+        {
+            //Creazione del client
+            var client = CreatePtzClient(ptzEndpoint);
+
+            //Definizione dei parametri
+            var vector = new PTZVector {
+                PanTilt = new Proxies.Ptz.Vector2D { x = x, y = y },
+                Zoom = new Proxies.Ptz.Vector1D { x = zoom }
+            };
+            var speed = new Proxies.Ptz.PTZSpeed {
+                PanTilt = new Proxies.Ptz.Vector2D { x = 1.0f, y = 1.0f },
+                Zoom = new Proxies.Ptz.Vector1D { x = 1.0f }
+            };
+
+            //Invocazione dell'operazione esposta dal servizio
+            await client.AbsoluteMoveAsync(profileName, vector, speed);
+        }
+
+        private static DeviceClient CreateDeviceClient(string deviceEndpoint)
+        {
+            //A
+            var address = new EndpointAddress(deviceEndpoint);
+            //B
+            var binding = CreateBinding();
+            //C
+            var client = new DeviceClient(binding, address);
+
+            return client;
+        }
+
+        private static MediaClient CreateMediaClient(string mediaEndpoint)
+        {
+            //A
+            var address = new EndpointAddress(mediaEndpoint);
+            //B
+            var binding = CreateBinding();
+            //C
+            var client = new MediaClient(binding, address);
+
+            return client;
+        }
+
+        private static PTZClient CreatePtzClient(string ptzEndpoint)
+        {
+            //A
+            var address = new EndpointAddress(ptzEndpoint);
+            //B
+            var binding = CreateBinding();
+            //C
+            var client = new PTZClient(binding, address);
+
+            return client;
+        }
+
+        private static Binding CreateBinding()
+        {
+            return new CustomBinding(new BindingElement[] { new TextMessageEncodingBindingElement(MessageVersion.Soap12WSAddressing10, Encoding.UTF8), new HttpTransportBindingElement() });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,44 @@
+# Demo ONVIF
+Questo progetto usa delle classi proxy generate a partire dai file WSDL della specifica ONVIF che sono elencati su https://www.onvif.org/profiles/specifications/
+
+La specifica ONVIF è formata di vari documenti WSDL, uno per ciascun modulo. Non bisogna referenziarli tutti ma solo quelli necessari all'applicazione (ad esempio [Device](https://www.onvif.org/ver10/device/wsdl/devicemgmt.wsdl) e [PTZ](https://www.onvif.org/ver20/ptz/wsdl/ptz.wsdl)).
+
+## Generazione delle classi proxy
+In questo progetto demo, le classi proxy sono già state generate. Per chi è curioso di sapere come sono state generate, ecco le istruzioni.
+
+Per prima cosa, installare il tool globale `svcutil` eseguendo questo comando:
+```
+dotnet tool install --global dotnet-svcutil
+```
+
+A questo punto eseguire il seguente comando dalla directory principale di questo progetto per generare le classi proxy. In questo caso viene usato il WSDL del modulo `Device` che consente di scoprire quali servizi sono offerti dalla telecamera. Il comando genererà il file [Proxies/Device.cs](Proxies/Device.cs) che contiene la classe [DeviceClient](Proxies/Device.cs#L16919).
+```
+dotnet svcutil https://www.onvif.org/ver10/device/wsdl/devicemgmt.wsdl --outputDir Proxies --outputFile Device.cs --namespace *,OnvifDemo.Proxies.Device
+```
+
+Aggiungere anche il modulo `Media` per ottenere i nomi dei profili esposti dalla telecamera. Il comando genererà il file [Proxies/Media.cs](Proxies/Media.cs) che contiene la classe [MediaClient](Proxies/Media.cs#L15538).
+
+```
+dotnet svcutil https://www.onvif.org/ver10/media/wsdl/media.wsdl --verbosity Silent --outputDir Proxies --outputFile Media.cs --namespace *,OnvifDemo.Proxies.Media
+```
+
+Infine, ecco il comando per aggiungere anche il proxy per il modulo `PTZ` che consente di spostare l'inquadratura della telecamera. Il comando genererà il file [Proxies/Ptz.cs](Proxies/Ptz.cs) che contiene la classe [PTZClient](Proxies/Ptz.cs#L12530).
+```
+dotnet svcutil https://www.onvif.org/ver20/ptz/wsdl/ptz.wsdl --outputDir Proxies --outputFile Ptz.cs --namespace *,OnvifDemo.Proxies.Ptz
+```
+
+
+## Utilizzo delle classi proxy
+In questo progetto è installato il pacchetto NuGet `System.ServiceModel.Http` con il seguente comando:
+```
+dotnet add package System.ServiceModel.Http
+```
+
+Poi, vedere il codice la classe [Program](Program.cs) su come usare le varie classi `DeviceClient`, `MediaClient` e `PTZClient`. 
+
+## Configurazione
+Bisogna giusto modificare il device endpoint della propria telecamera dalla linea 14 del file [Program.cs](Program.cs#L13).
+
+## Software che può tornare utile
+ * Client ONVIF: [ONVIF Device Manager](https://sourceforge.net/projects/onvifdm/)
+ * Server ONVIF [Happytime Multi ONVIF Server](http://www.happytimesoft.com/products/multi-onvif-server/index.html)
